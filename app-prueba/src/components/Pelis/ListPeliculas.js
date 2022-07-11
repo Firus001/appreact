449188a3d5faa59d524cb78eba7f14cad3655848
Firus001 <74003744+Firus001@users.noreply.github.com>
2022-07-10 21:16:39 -0300

    Delete app-prueba directory

--- app-prueba/src/components/Pelis/ListPeliculas.js
+++
@@ -1,7 +0,0 @@
-export default function ListPeliculas ({ peliculas }){
-    const peliculasComponents = peliculas.mp((pelicula,i)=>{
-        <div key={1}>
-        Nombre: {pelicula.nombre}.Añio:{pelicula.anioDeEstreno}
-        </div>
-    })
-}
